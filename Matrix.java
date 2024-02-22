cbadbbafafc0fba3f43249705ca02496bb5394a3
nilmar pesaras <112700781+nilmar-pesaras@users.noreply.github.com>
2024-02-22 11:28:00 +0800

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,3 +1,12 @@
+/*
+Pesaras, Nilmar T.
+ITE114 - IT2D.1
+MACHINE PROBLEM 1 - INFIX TO POSTFIX CONVERSION AND STACK OPERATIONS
+
+This program deals with Matrices using multidimensional arrays.
+This program applies the Matrix theory in its basic operations. The operations also uses concepts of multidimensional array.
+ */
+
 import java.util.Scanner;
 public class Matrix {
     public static void main (String [] args){
@@ -169,56 +178,59 @@ public class Matrix {
                     }
                 } else if (operation == '3') {
                     try {
-                        System.out.print("Enter the constant to be multiplied to the matrices: ");
-                        int m_constant = user_input.nextInt();
+                        boolean conformable;
+                        while (conformable = true){
+                            System.out.print("Would you like to create another matrix for Scalar Multiplication? (Y/N): ");
+                            char response = user_input.next().charAt(0);
 
-                        for (int i = 0; i < matrixNum; i++) {
-                            System.out.println("The Elements of Matrix " + (i + 1) + " are:");
-                            outputMatrix(matrices[i]);
-
-                            result = new int[matrices[i].length][matrices[i][0].length];
-                            for (int j = 0; j < matrices[i].length; j++) {
-                                for (int k = 0; k < matrices[i][0].length; k++) {
-                                    result[j][k] = m_constant * matrices[i][j][k];
-                                }
+                            if (response == 'Y' || response == 'y') {
+                                do {
+                                    int[][] newMatrix = add_newMatrixScalar(matrices[0]);
+                                    conformable = checkConformabilityForScalar(newMatrix);
+                                    if (!conformable) {
+                                        System.out.println("Entered matrices are not conformable. Please try again!");
+                                    }
+                                } while (!conformable);
+                            } else if (response == 'N' || response == 'n') {
+                                break;
+                            } else {
+                                System.out.println("Input invalid, input Y/N only. Please try again!");
                             }
-                            System.out.println("The product of the matrix and the constant is: ");
-                            outputMatrix(result);
                         }
                     } catch (IllegalArgumentException e) {
                         System.out.println(e.getMessage());
                     }
                 } else if (operation == '4') {
                     try {
-                        for (int i = 0; i < matrixNum; i++) {
-                            result = new int[matrices[i].length][matrices[i][0].length];
-                            for (int j = 0; j < matrices[i].length; j++) {
-                                for (int k = 0; k < matrices[i][0].length; k++) {
-                                    if (j == k) {
-                                        result[j][k] = matrices[i][j][k];
-                                    }else {
-                                        result[j][k] = 0;
+                        boolean conformable;
+                        while (conformable = true){
+                            System.out.print("Would you like to create another matrix for diagonalization? (Y/N): ");
+                            char response = user_input.next().charAt(0);
+
+                            if (response == 'Y' || response == 'y') {
+                                do {
+                                    int[][] newMatrix = add_newMatrixDiag(matrices[0]);
+                                    conformable = checkConformabilityForDiagonalization(newMatrix);
+                                    if (!conformable) {
+                                        System.out.println("Entered matrices are not conformable. Please try again!");
                                     }
-                                }
+                                } while (!conformable);
+                            } else if (response == 'N' || response == 'n') {
+                                break;
+                            } else {
+                                System.out.println("Input invalid, input Y/N only. Please try again!");
                             }
-                            System.out.println();
-                            System.out.println("The diagonalized matrix is: ");
-                            outputMatrix(result);
                         }
+
                     } catch (IllegalArgumentException e) {
                         System.out.println(e.getMessage());
                     }
+
                 } else {
                     System.out.println("Please choose a valid number based on the choices");
                     continue;
                 }
-
-                // This will ask if the user wants to perform another operation using the same matrices that they input.
-                System.out.print("Would you like to make use of the same matrices to perform another operation? (Y/N): ");
-                char answer2 = user_input.next().charAt(0);
-                if (answer2 == 'N' || answer2 == 'n') {
-                    break;
-                }
+                break;
             }
         }catch (Exception e){
             System.out.println("Input invalid. Please try again!");
@@ -352,6 +364,130 @@ public class Matrix {
         return product;
     }
 
+    public static int[][] add_newMatrixScalar(int[][] scalar) {
+        Scanner user_input = new Scanner(System.in);
+        int rows_num, cols_num;
+        boolean conformable = false;
+
+        try{
+                System.out.print("\nEnter Matrix Name: ");
+                char name = user_input.next().charAt(0);
+
+                System.out.print("Enter a constant to be used for the Scalar Multiplication of the matrix: ");
+                int scalarValue = user_input.nextInt();
+
+                System.out.print("Number of rows: ");
+                rows_num = user_input.nextInt();
+                System.out.print("Number of columns: ");
+                cols_num = user_input.nextInt();
+
+                if (rows_num > 10 || cols_num > 10) {
+                    System.out.println("Matrix Dimension limit is at 10x10. Please try again!");
+                } else if (rows_num <= 0 || cols_num <= 0) {
+                    System.out.println("Matrix Dimension must be greater than zero. Please try again!");
+                } else {
+
+                    int[][] newMatrix = new int[rows_num][cols_num];
+
+                    System.out.println("Enter the elements of the new matrix: ");
+                    for (int i = 0; i < rows_num; i++) {
+                        for (int k = 0; k < cols_num; k++) {
+                            try {
+                                System.out.print("Enter Row " + (i + 1) + ", Column " + (k + 1) + " value: ");
+                                newMatrix[i][k] = user_input.nextInt();
+                            } catch (Exception e){
+                                System.out.println ("Invalid Input! Please enter a number: ");
+                                user_input.next();
+                                k--;
+                            }
+                        }
+                    }
+
+                    System.out.println("\nThe Elements of Matrix " + (name) + " are:");
+                    outputMatrix(newMatrix);
+
+                    int[][] scalarizeMatrix = scalarMulti(newMatrix, scalarValue);
+
+                    System.out.println("\nNew Scalar Matrix:");
+                    outputMatrix(scalarizeMatrix);
+
+                    if (scalarizeMatrix != null){
+                        int[][] scalar_matrix = scalarizeMatrix;
+                        conformable = true;
+                    } else {
+                        System.out.println("The given matrices are not conformable for diagonalization. Please try again!");
+                    }
+                }
+        } catch (Exception e){
+            System.out.println ("Invalid Input!");
+        }
+        return scalar;
+    }
+
+    public static int[][] add_newMatrixDiag(int[][] diagonal) {
+        Scanner user_input = new Scanner(System.in);
+        int rows_num, cols_num;
+        boolean conformable = false;
+
+        try{
+            System.out.print("\nEnter Matrix Name: ");
+            char name = user_input.next().charAt(0);
+
+            System.out.print("Enter a constant to be used for the Diagonalization of the matrix: ");
+            int diagConstant = user_input.nextInt();
+
+            System.out.print("Number of rows: ");
+            rows_num = user_input.nextInt();
+            System.out.print("Number of columns: ");
+            cols_num = user_input.nextInt();
+
+            if (rows_num > 10 || cols_num > 10) {
+                System.out.println("Matrix Dimension limit is at 10x10. Please try again!");
+            } else if (rows_num <= 0 || cols_num <= 0) {
+                System.out.println("Matrix Dimension must be greater than zero. Please try again!");
+            } else {
+
+                if (rows_num == cols_num) {
+                    int[][] newMatrix = new int[rows_num][cols_num];
+
+                    System.out.println("Enter the elements of the new matrix: ");
+                    for (int i = 0; i < rows_num; i++) {
+                        for (int k = 0; k < cols_num; k++) {
+                            try {
+                                System.out.print("Enter Row " + (i + 1) + ", Column " + (k + 1) + " value: ");
+                                newMatrix[i][k] = user_input.nextInt();
+                            } catch (Exception e){
+                                System.out.println ("Invalid Input! Please enter a number: ");
+                                user_input.next();
+                                k--;
+                            }
+                        }
+                    }
+
+                    System.out.println("\nThe Elements of Matrix " + (name) + " are:");
+                    outputMatrix(newMatrix);
+
+                    int[][] diagonalMatrix = diagonalize(newMatrix, diagConstant);
+
+                    System.out.println("\nNew Diagonal Matrix:");
+                    outputMatrix(diagonalMatrix);
+
+                    if (diagonalMatrix != null){
+                        int[][] diag = diagonalMatrix;
+                        conformable = true;
+                    } else {
+                        System.out.println("The given matrices are not conformable for diagonalization. Please try again!");
+                    }
+                } else {
+                    System.out.println("The given matrices are not conformable for diagonalization. Please try again!");
+                }
+            }
+        } catch (Exception e){
+            System.out.println ("Invalid Input!");
+        }
+        return diagonal;
+    }
+
     // Method checkConformabilityForAddition() checks if two matrices can be added by comparing their dimensions.
     public static boolean checkConformabilityForAddition(int[][] matrix1, int[][] matrix2) {
         return matrix1.length == matrix2.length && matrix1[0].length == matrix2[0].length;
@@ -361,6 +497,30 @@ public class Matrix {
     // by comparing the number of columns in the first matrix with the number of rows in the second matrix.
     public static boolean checkConformabilityForMultiplication(int[][] matrix1, int[][] matrix2) {
         return matrix1[0].length == matrix2.length;
+    }
+
+    public static boolean checkConformabilityForDiagonalization(int[][] matrix1) {
+        int[][] checkDiag = new int[matrix1.length][matrix1[0].length];
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[0].length; j++) {
+                if (i == j) {
+                    checkDiag[i][j] = matrix1[i][j];
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkConformabilityForScalar(int[][] matrix1) {
+        int[][] checkScalar = new int[matrix1.length][matrix1[0].length];
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[0].length; j++) {
+                if (i == j) {
+                    checkScalar[i][j] = matrix1[i][j];
+                }
+            }
+        }
+        return true;
     }
 
     // Method add() receives an array of matrices and returns their sum.
@@ -385,6 +545,27 @@ public class Matrix {
             }
         }
         return p;
+    }
+
+    public static int[][] diagonalize(int[][] newMatrix, int diagConstant){
+        int[][] diagonalMatrix = new int [newMatrix.length][newMatrix[0].length];
+        for(int i=0; i<newMatrix.length; i++){
+            for(int j=0; j<newMatrix[i].length; j++){
+                if(i==j){
+                    diagonalMatrix[i][j] = diagConstant;
+                }else{
+                    diagonalMatrix[i][j] = newMatrix[i][j];
+                }
+            }
+        } return diagonalMatrix;
+    }
+    public static int[][] scalarMulti(int[][] newMatrix, int scalarConstant){
+        int[][] scalarMatrix = new int [newMatrix.length][newMatrix[0].length];
+        for(int i=0; i<newMatrix.length; i++){
+                for(int j=0; j<newMatrix[i].length; j++){
+                    scalarMatrix[i][j] = newMatrix[i][j] * scalarConstant;
+                }
+        } return scalarMatrix;
     }
 
     // Method add() overloaded version that adds two matrices together and returns their sum.
